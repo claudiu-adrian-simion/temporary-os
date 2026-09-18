079d09d42cff110213e298a97c323d11e51e61b1
claudiu-adrian-simion <claudiuadrian.simion@gmail.com>
2026-09-18 12:35:08 +0200

    removed packeges

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,5 +7,26 @@ cp -avf "/ctx/system_files"/. /
 # this installs a package from fedora repos
 dnf5 install -y just
 
+# Remove unecessary packages
+# Firefox
+dnf5 remove -y firefox firefox-langpacks
+
+# Vim
+dnf5 remove -y vim-minimal
+
+# Fedora configs
+dnf5 remove -y fedora-workstation-backgrounds fedora-bookmarks fedora-chromium-config
+
+# CLI Tools replacable by homebrew
+dnf5 remove -y nano nano-default-editor default-editor
+dnf5 remove -y bc
+dnf5 remove -y tree
+dnf5 remove -y rsync
+dnf5 remove -y zip
+dnf5 remove -y lsof
+dnf5 remove -y mtr
+dnf5 remove -y whois
+dnf5 remove -y wget2-wget
+
 dnf5 clean all
 systemctl enable podman.socket
