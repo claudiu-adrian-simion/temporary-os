945b84dad5ac0cb1c9568ef9a13e7fa4286f7ea1
claudiu-adrian-simion <claudiuadrian.simion@gmail.com>
2026-09-18 12:54:08 +0200

    moved git to homebrew and added dependecies for it

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,6 +6,7 @@ cp -avf "/ctx/system_files"/. /
 
 # this installs a package from fedora repos
 dnf5 install -y just
+dnf5 install -y development-tools
 
 # Remove unecessary packages
 # Firefox
@@ -27,6 +28,7 @@ dnf5 remove -y lsof
 dnf5 remove -y mtr
 dnf5 remove -y whois
 dnf5 remove -y wget2-wget
+dnf5 remove -y git-core git-core-doc
 
 dnf5 clean all
 systemctl enable podman.socket
